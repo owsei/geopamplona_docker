91c70c4f8a904b88739c2d3a423354036285d210
Pedro <owsei@yahoo.com>
2025-05-16 00:40:31 +0200

    geoserver frontal

---
+++ geoserver/geoserver-visor/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MapViewer from './components/MapViewer';
+
+function App() {
+  return (
+    <div className="App">
+      <MapViewer /> 
+    </div>
+  );
+}
+
+export default App;
